cb933c9897193fc5e21ea493ef6cbc005d8e9df9
Bhupendra Dewangan <71768549+bhupendra-dew@users.noreply.github.com>
2023-11-18 22:44:58 +0530

    learn

---
+++ JavaScript/Learning/learning1.js
@@ -0,0 +1,7 @@
+let num = 20;
+if (num % 2 == 0) {
+    document.write("Given number is an even number.");
+}
+if (num % 2 !== 0){
+    document.write("The given number is an odd number.")
+}
--- Python/Class/OOP.py
+++ Python/Class/OOP.py
@@ -70,7 +70,7 @@ class Student:
 student1 = Student("Ashu", "Dewangan", 17)
 student1.getStudentDetails()
 '''
-
+'''
 class MyClass:
     __hiddenVariable = 0
     def add(self, increment):
@@ -80,3 +80,90 @@ myObject = MyClass()
 myObject.add(2)
 myObject.add(5)
 print(myObject.__hiddenVariable)
+'''
+'''
+class Dog:
+    
+    attr1 = 'mammal'
+    attr2 = 'dog'
+    attr3 = 'bark'
+
+    def fun(self):
+        print("I am a", self.attr1)
+        print("I am a", self.attr2)
+
+    def speak(self):
+        print("I can", self.attr3)
+
+Rodger = Dog()
+print(Rodger.attr1)
+Rodger.fun()
+#Rodger.speak()
+'''
+'''
+class Ashu:
+    def __init__(self, name, company) -> None:
+        self.name = name
+        self.company = company
+        pass
+
+    def show(self):
+        print("Hello my name is", self.name, "and I work in", self.company+".")
+
+object = Ashu("Bhupendra", "Epic Games")
+object.show()
+'''
+'''
+class Ashu:
+    def __init__(BD, name, company) -> None:
+        BD.name = name
+        BD.company = company
+        pass
+
+    def show(BD):
+        print("Hello my name is", BD.name, "and I work in", BD.company + ".")
+
+obj = Ashu("Bhupendra", "Ubisoft")
+obj.show()
+'''
+
+# Self
+'''
+class mynumber:
+    def __init__(self, value) -> None:
+        self.value = value
+        pass
+
+    def print_value(self):
+        print(self.value)
+
+object1 = mynumber(int(input("Enter the number:")))
+object1.print_value()
+'''
+'''
+class Subject:
+    def __init__(self, attr1, attr2) -> None:
+        self.attr1 = attr1
+        self.attr2 = attr2
+        pass
+
+object = Subject('Maths', 'Science')
+print(object.attr1)
+print(object.attr2)
+'''
+
+
+class Cars():
+    def __init__(self, company, model, color) -> None:
+        self.company = company
+        self.model = model
+        self.color = color
+        pass
+
+    def show(self):
+        print("The Company is :", self.company)
+        print("The model is :", self.model)
+        print("The Color is :", self.color)
+
+car_no = int(input("Enter the number of cars you wnat to store: "))
+
