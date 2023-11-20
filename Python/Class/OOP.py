7adeefcbb52abbfaf4b92a8ee170016638022b81
Bhupendra Dewangan <71768549+bhupendra-dew@users.noreply.github.com>
2023-11-20 22:43:02 +0530

    Update OOP.py

--- Python/Class/OOP.py
+++ Python/Class/OOP.py
@@ -278,8 +278,208 @@ print(dog.breed)
 '''
 
 # Creating a property
-
+'''
 class Circle:
     def __init__(self, radius) -> None:
-        self.radius = radius
+        self._radius = radius
         pass
+    @property
+    def radius(self):
+        return self._radius
+    @radius.setter
+    def radius(self, value):
+        if value >= 0:
+            self._radius = value
+        else:
+            raise ValueError("Radius must be positive")
+
+circle = Circle(5)
+print(circle.radius)
+circle.radius = 10
+print(circle.radius)
+'''
+
+# Encapsulation - Private members
+'''
+class MyClass:
+    def __init__(self) -> None:
+        self.public = "Public"
+        self._protected = "Protected"
+        self.__private = "Private"
+        pass
+
+obj = MyClass()
+print(obj.public)
+print(obj._protected)
+#print(obj.__private)
+'''
+
+# Polymorphism - Using Inbuilt Abstract Base Classes(ABC)
+'''
+from collections.abc import Iterable
+
+def get_length(item):
+    if isinstance(item, Iterable):
+        return len(item)
+    else:
+        return "Not iterable"
+    
+print(get_length("Hello"))
+print(get_length([1, 2, 3]))
+print(get_length(123))
+'''
+
+# Defining an Abstract Base Class(ABC)
+'''
+from abc import ABC, abstractmethod
+class AbstractAnimal(ABC):
+    @abstractmethod
+    def speak(self):
+        pass
+
+class Dog(AbstractAnimal):
+    def speak(self):
+        return "Boww Boww!!!"
+    
+dog = Dog()
+print(dog.speak())
+'''
+
+# Using class methods and static methods
+'''
+class MyClass:
+    @classmethod
+    def class_method(cls):
+        return "Class method called"
+    @staticmethod
+    def static_method():
+        return "Static method called"
+    
+print(MyClass.class_method())
+print(MyClass.static_method())
+'''
+
+# Operator Overloading in Python
+'''
+class Mango:
+    def __init__(self, x) -> None:
+        self.x = str(x)
+        pass
+
+    def __add__(self, y):
+        return self.x + y.x
+    
+obj1 = Mango(140)
+obj2 = Mango('mangoes')
+print(obj1 + obj2)
+'''
+
+# Using Special methods for string representations(repr and str)
+'''
+class Person:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+        pass
+
+    def __str__(self) -> str:
+        return f"{self.name} is {self.age} years old."
+    
+    def __repr__(self) -> str:
+        return f"Person('{self.name}', '{self.age})"
+    
+p = Person("Bob", 30)
+print(str(p))
+print(repr(p))
+'''
+
+# Using composite in Pyhton OOP
+'''
+class Salary:
+    def __init__(self, pay, bonus) -> None:
+        self.pay = pay
+        self.bonus = bonus
+        pass
+
+class Employee:
+    def __init__(self, name, salary) -> None:
+        self.name = name
+        self.salary = salary
+        pass
+
+s = Salary(15000, 5000)
+e = Employee("Ashwin", s)
+print(e.salary.pay)
+print(e.salary.bonus)
+'''
+
+# Using multiple inheritance
+'''
+class Parent1:
+    def method1(self):
+        return "Parent1's method called"
+
+class Parent2:
+    def method2(self):
+        return "Parent2's method called"
+    
+class Child(Parent1, Parent2):
+    pass
+
+c = Child()
+print(c.method1())
+print(c.method2())
+'''
+
+# Implementing Decorators within classes
+'''
+class MyClass:
+    @staticmethod
+    def method():
+        return "Static method called"
+    
+    @classmethod
+    def classmethod(cls):
+        return "Class method called"
+    
+print(MyClass.method())
+print(MyClass.classmethod())
+'''
+
+# Creating a Singleton class in Python
+'''
+class Singleton:
+    _instance = None
+
+    @staticmethod
+    def getInstance():
+        if Singleton._instance == None:
+            Singleton()
+        return Singleton._instance
+    
+    def __init__(self) -> None:
+        if Singleton._instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton._instance = self
+        pass
+
+s = Singleton.getInstance()
+print(s)
+'''
+
+# Using Mixin classes in Python
+
+class Mixin1(object):
+    def test(self):
+        print("Mixin1")
+
+class Mixin2(object):
+    def test(self):
+        print("Mixin2")
+
+class MyClass(Mixin1, Mixin2):
+    pass
+
+obj = MyClass()
+obj.test()
