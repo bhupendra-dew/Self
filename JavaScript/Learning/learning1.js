3ede6f3df75e47faccae2a50e3bea5c2e0fafe61
Bhupendra <bhupendradew2003@gmail.com>
2023-12-11 15:37:54 +0530

    learning

--- JavaScript/Learning/learning1.js
+++ JavaScript/Learning/learning1.js
@@ -1,5 +1,51 @@
+/*
 function sum(a, b) {
     return a + b;
 }
+let x = prompt("Enter the first numeber: ")
+let y = prompt("Enter the second numeber: ")
+document.write("The sum is : ", sum(x, y));  // Outputs: 8
+*/
+/*
 
-document.write(sum(5, 3));  // Outputs: 8
+let arr1 = new Array(3)
+arr1[0] = 10
+arr1[1] = 20
+arr1[2] = 30
+document.write("\nArray 1 : ", arr1)
+let arr2 = new Array(10, 20 ,30, 40, 50);
+document.write("\nArray 2 : ", arr2)
+let arr3 = new Array(5);
+document.write("\nArray 3 : ", arr3)
+let arr4 = new Array("ASHU");
+document.write("\nArray 4 : ", arr4)
+*/
+/*
+const courses = ["HTML", "CSS", "Javascript"];
+document.write(courses[0])
+document.write(courses[1])
+document.write(courses[2])
+*/
+/*
+const courses = ["HTML", "CSS", "Javascript"];
+document.write(courses);
+
+document.write("<br />")
+
+courses[1] = "GeeksforGeeks"
+document.write(courses);
+*/
+/*
+
+
+const courses = ["HTML", "CSS", "Javascript"]
+courses.length = 5
+document.write("Array after increasing the length : ", courses)
+
+document.write("<br \>")
+
+courses[3] = "php"
+courses[4] = "React"
+document.write("Array after initializing: ", courses)
+*/
+
